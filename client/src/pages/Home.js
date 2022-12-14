9d7d12987b7c95eca8bf2af4adde9647879826a1
Dan Klaver <dklaver15@gmail.com>
2022-12-14 14:09:45 -0600

    Added route for home

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+// import React, { useState } from "react";
+// import { useParams } from "react-router-dom";
+// import axios from "axios";
+
+function Home() {
+  return (
+    <>
+        <div className="flex space-x-3">
+          Hello World
+        </div>
+    </>
+  );
+};
+
+export default Home;
