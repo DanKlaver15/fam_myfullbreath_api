6afae54a2cba6c6fd20d100b7de685844184f281
Dan Klaver <dklaver15@gmail.com>
2022-12-14 14:34:55 -0600

    Added password reset page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home.js';
 import VerifyEmail from './pages/VerifyEmail.js';
+import ResetPassword from './pages/ResetPassword.js';
 
 function App() {
   return (
     <div className="App">
 			<Router>
 				<Routes>
-					<Route path="/verifyEmail" element={<VerifyEmail />} />
 					<Route path="/" element={<Home />} />
+					<Route path="/verifyEmail" element={<VerifyEmail />} />
+					<Route path="/resetPassword" element={<ResetPassword />} />
 				</Routes>
 			</Router>
     </div>
