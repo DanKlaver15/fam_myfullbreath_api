5c86f6442e8afbd101cfd754b99aa30c41ed2df6
Dan Klaver <dklaver15@gmail.com>
2022-12-14 18:16:39 -0600

    Moved app icon location

--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -37,7 +37,7 @@ function UpdatePassword() {
 				<div className="sm:mx-auto sm:w-full sm:max-w-md">
 					<img
 						className="mx-auto h-24 w-auto rounded-lg"
-						src="../../public/App_Icon.png"
+						src="../images/App_Icon.png"
 						alt="Fight Asthma Milwaukee Allies"
 					/>
 					<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-200">
