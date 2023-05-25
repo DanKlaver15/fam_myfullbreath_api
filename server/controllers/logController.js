937ce6e796607b4cfec0c271a6dca1eaab8dab07
Dan Klaver <dklaver15@gmail.com>
2023-05-25 16:46:01 -0500

    chore(logController.js): add console.log statement to log newLog object before saving it to CSV file for debugging purposes

--- server/controllers/logController.js
+++ server/controllers/logController.js
@@ -3,6 +3,7 @@ const Log = require('../models/log');
 const addToLog = (req, res) => {
 	try {
 		const newLog = new Log(req.body);
+		console.log(newLog);
 		newLog.saveAsCSV();
 		return res.status(200).send('Log saved');
 	} catch (err) {
