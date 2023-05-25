52ac5b0f14747d39911e0b18842cdd6d112a3096
Dan Klaver <dklaver15@gmail.com>
2023-05-25 16:40:52 -0500

    refactor(logController.js): add try-catch block to handle errors when saving logs to CSV file feat(logController.js): return HTTP status codes and messages to indicate success or failure of log saving operation

--- server/controllers/logController.js
+++ server/controllers/logController.js
@@ -1,8 +1,14 @@
 const Log = require('../models/log');
 
-const addToLog = (req) => {
-	const newLog = new Log(req.body);
-	newLog.saveAsCSV();
+const addToLog = (req, res) => {
+	try {
+		const newLog = new Log(req.body);
+		newLog.saveAsCSV();
+		return res.status(200).send('Log saved');
+	} catch (err) {
+		return res.status(500).send(err);
+	}
+	
 };
 
 module.exports = { addToLog };
